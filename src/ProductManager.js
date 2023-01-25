7aed94b197368164f33f954084007397773e237c
fedeeribeiro <ribeirofede@gmail.com>
2023-01-24 22:43:04 -0300

    finished challenge

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
+import fs from 'fs';
 
-class productManager {
-    constructor(){
-        this.path = './products.json';
-        fs.writeFileSync(this.path, JSON.stringify([]))
+export default class ProductManager {
+    constructor(path){
+        this.path = path
     }
 
-    #setId = () => {
-        const productsArray = JSON.parse(fs.readFileSync(this.path, 'utf-8'));
+    #setId = async () => {
+        const productsFromFile = await fs.promises.readFile(this.path, 'utf-8');
+        const productsArray = JSON.parse(productsFromFile);
         if(productsArray.length !== 0){
             let id = 0;
             productsArray.forEach(product => {
@@ -21,15 +21,20 @@ class productManager {
         }
     }
 
-    #isCodeInUse = (code) => JSON.parse(fs.readFileSync(this.path, 'utf-8')).find(product => product.code === code); 
+    #isCodeInUse = async (code) => {
+        const productsFromFile = await fs.promises.readFile(this.path, 'utf-8');
+        const productsArray = JSON.parse(productsFromFile);
+        return productsArray.find(product => product.code === code)
+    } 
 
-    addProduct(title, description, price, thumbnail, code, stock){
-        if (!title || !description || !price || !thumbnail || !code || !stock){
+    async addProduct(title, description, price, thumbnail, code, stock){
+        if(!title || !description || !price || !thumbnail || !code || !stock){
             console.log('Error. No se pueden dejar campos vacíos al agregar un nuevo producto.')
-        }else if (this.#isCodeInUse(code)) console.log('Error. No se pueden ingresar dos productos con el mismo code.')
-        else{
+        // }else if(this.#isCodeInUse(code)){
+        //     console.log('Error. No se pueden ingresar dos productos con el mismo code.')    
+        }else{
             const product = {
-                id: this.#setId(),
+                id: await this.#setId(),
                 title,
                 description,
                 price,
@@ -37,31 +42,36 @@ class productManager {
                 code,
                 stock
             }
-            const productsArray = JSON.parse(fs.readFileSync(this.path, 'utf-8'));
+            const productsFromFile = await fs.promises.readFile(this.path, 'utf-8');
+            const productsArray = JSON.parse(productsFromFile);
             productsArray.push(product);
-            fs.writeFileSync(this.path, JSON.stringify(productsArray))
+            await fs.promises.writeFile(this.path, JSON.stringify(productsArray));
             console.log('Se ha agregado el producto exitosamente.')
         }
     }
 
-    getProducts(){
-        const productsArray = JSON.parse(fs.readFileSync(this.path, 'utf-8'));
-        console.log('Estos son los productos que se encuentran en el array:');
-        console.log(productsArray)
+    async getProducts(queries){
+        const { limit } = queries;
+        const productsFromFile = await fs.promises.readFile(this.path, 'utf-8');
+        const productsArray = JSON.parse(productsFromFile);
+        if (!productsArray) console.log('Error. Not found.')
+        else{
+            if (limit) return productsArray.slice(0, limit)
+            else return productsArray
+        }    
     }
 
-    getProductById(id){
-        const productsArray = JSON.parse(fs.readFileSync(this.path, 'utf-8'));
+    async getProductById(id){
+        const productsFromFile = await fs.promises.readFile(this.path, 'utf-8');
+        const productsArray = JSON.parse(productsFromFile);
         const productFound = productsArray.find(product => product.id === id);
         if (!productFound) console.log('Error. Not found.')
-        else {
-            console.log('Se ha encontrado el siguiente producto:');
-            console.log(productFound)
-        }
+        else return productFound
     }
 
-    updateProduct(id, prop, value){
-        const productsArray = JSON.parse(fs.readFileSync(this.path), 'utf-8');
+    async updateProduct(id, prop, value){
+        const productsFromFile = await fs.promises.readFile(this.path, 'utf-8');
+        const productsArray = JSON.parse(productsFromFile);
         const productFound = productsArray.find(product => product.id === id);
         if(productFound){
             if(prop === 'title'){
@@ -85,54 +95,21 @@ class productManager {
             }else{
                 console.log('Error. Se ha ingresado una propiedad no válida.')
             }
-            fs.writeFileSync(this.path, JSON.stringify(productsArray))
+            await fs.promises.writeFile(this.path, JSON.stringify(productsArray))
         }else{
             console.log('Error. No se ha encontrado ningún producto con el id ingresado.')
         }
     }
 
-    deleteProduct(id){
-        const productsArray = JSON.parse(fs.readFileSync(this.path), 'utf-8');
+    async deleteProduct(id){
+        const productsFromFile = await fs.promises.readFile(this.path, 'utf-8');
+        const productsArray = JSON.parse(productsFromFile);
         if(productsArray.find(product => product.id === id)){
             const newProductsArray = productsArray.filter(product => product.id !== id);
-            fs.writeFileSync(this.path, JSON.stringify(newProductsArray))
+            await fs.promises.writeFile(this.path, JSON.stringify(newProductsArray))
             console.log('Se ha eliminado el producto exitosamente.')
         }else{
             console.log('Error. No se ha encontrado ningún producto con el id ingresado.')
         }
     }
 }
-
-//              *****************************TESTING*****************************             
-
-const productManagerTester = new productManager();
-
-productManagerTester.getProducts();
-
-productManagerTester.addProduct('producto prueba', 'este es un producto prueba', 200, 'sin imagen', 'abc123', 25);
-
-productManagerTester.addProduct('producto prueba', 'este es un producto prueba', 200, 'sin imagen', 'abc123', 25);
-
-productManagerTester.addProduct('segundo producto prueba 222', 'este es un producto prueba', 200, 'sin imagen', 'abc143', 25);
-
-productManagerTester.getProducts();
-
-productManagerTester.getProductById(1);
-
-productManagerTester.getProductById(7);
-
-productManagerTester.updateProduct(1, 'title', 'nuevo producto prueba con nombre modificado');
-
-productManagerTester.updateProduct(4, 'title', 'producto inexistente');
-
-productManagerTester.getProductById(1);
-
-productManagerTester.deleteProduct(1);
-
-productManagerTester.deleteProduct(9);
-
-productManagerTester.getProducts();
-
-productManagerTester.addProduct('tercer producto prueba 33', 'este es un producto prueba', 200, 'sin imagen', 'rteo1123', 25);
-
-productManagerTester.getProducts();
